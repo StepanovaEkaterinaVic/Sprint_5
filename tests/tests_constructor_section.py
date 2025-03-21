1eea59b20377646f7c20372a5aa4d9375e757b44
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-03-21 16:45:35 +0300

    Создан тестовый файл tests_constructor_section.py с тестовыми функциями: 1) test_navigate_to_sauces_section проверяет переход к разделу "Соусы" 2) test_navigate_to_fillings_section проверяет переход к разделу "Начинки" 3) test_navigate_to_bread_section проверяет переход к разделу "Булки"

---
+++ tests/tests_constructor_section.py
@@ -0,0 +1,31 @@
+from locators import Locators
+
+
+def verify_tab_state(driver, locator, expected_active):
+    class_attribute = driver.find_element(*locator).get_attribute("class")
+    if expected_active:
+        assert "tab_tab_type_current__2BEPc" in class_attribute
+    else:
+        assert "tab_tab_type_current__2BEPc" not in class_attribute
+
+
+def test_navigate_to_sauces_section(driver): # переход к разделу "Соусы"
+    driver.find_element(*Locators.SAUCE_SECTION).click()
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=False)
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=True)
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=False)
+
+
+def test_navigate_to_fillings_section(driver): # переход к разделу "Начинки"
+    driver.find_element(*Locators.FILLING_SECTION).click()
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=False)
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=False)
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=True)
+
+
+def test_navigate_to_bread_section(driver): # переход к разделу "Булки"
+    driver.find_element(*Locators.FILLING_SECTION).click()
+    driver.find_element(*Locators.BREAD_SECTION).click()
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=True)
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=False)
+    verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=False)
