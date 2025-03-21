b837cc4e3b7f52ad8c3875d61495fea8aaf4257e
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-03-21 16:25:21 +0300

    Создан файл curl.py с переменными, созданными для обращения к нужной странице сайта

---
+++ tests/curl.py
@@ -0,0 +1,5 @@
+main_site = 'https://stellarburgers.nomoreparties.site/'
+registration = 'https://stellarburgers.nomoreparties.site/register'
+entrance = 'https://stellarburgers.nomoreparties.site/login'
+profile = 'https://stellarburgers.nomoreparties.site/account/profile'
+
