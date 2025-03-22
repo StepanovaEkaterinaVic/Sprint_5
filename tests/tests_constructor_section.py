38491c39b73b3575249104f1c2585a9d4cc9bdc8
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-03-22 19:59:01 +0300

    В helper.py добавлена функция verify_tab_state для проверки состояния вкладок.

--- tests/tests_constructor_section.py
+++ tests/tests_constructor_section.py
@@ -9,23 +9,23 @@ def verify_tab_state(driver, locator, expected_active):
         assert "tab_tab_type_current__2BEPc" not in class_attribute
 
 
-def test_navigate_to_sauces_section(driver): # переход к разделу "Соусы"
-    driver.find_element(*Locators.SAUCE_SECTION).click()
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=False)
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=True)
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=False)
+class TestsNavigationSection:
 
+    def test_navigate_to_sauces_section(self, driver):  # переход к разделу "Соусы"
+        driver.find_element(*Locators.SAUCE_SECTION).click()
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=False)
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=True)
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=False)
 
-def test_navigate_to_fillings_section(driver): # переход к разделу "Начинки"
-    driver.find_element(*Locators.FILLING_SECTION).click()
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=False)
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=False)
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=True)
+    def test_navigate_to_fillings_section(self, driver):  # переход к разделу "Начинки"
+        driver.find_element(*Locators.FILLING_SECTION).click()
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=False)
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=False)
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=True)
 
-
-def test_navigate_to_bread_section(driver): # переход к разделу "Булки"
-    driver.find_element(*Locators.FILLING_SECTION).click()
-    driver.find_element(*Locators.BREAD_SECTION).click()
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=True)
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=False)
-    verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=False)
+    def test_navigate_to_bread_section(self, driver):  # переход к разделу "Булки"
+        driver.find_element(*Locators.FILLING_SECTION).click()
+        driver.find_element(*Locators.BREAD_SECTION).click()
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_BREAD_SECTION, expected_active=True)
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_SAUCE_SECTION, expected_active=False)
+        verify_tab_state(driver, Locators.SAUCE_ACTIVE_FILLING_SECTION, expected_active=False)
