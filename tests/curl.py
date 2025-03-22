0d90c43fa935028116d7b80d482db994a7256167
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-03-22 18:49:34 +0300

    В корневую директорию из папки tests перенесен файл curl.py

--- tests/curl.py
+++
@@ -1,5 +0,0 @@
-main_site = 'https://stellarburgers.nomoreparties.site/'
-registration = 'https://stellarburgers.nomoreparties.site/register'
-entrance = 'https://stellarburgers.nomoreparties.site/login'
-profile = 'https://stellarburgers.nomoreparties.site/account/profile'
-
