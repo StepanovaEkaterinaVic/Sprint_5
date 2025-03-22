f363256b891e83426b0fe1c9133498a2954aca49
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-03-22 19:00:29 +0300

    helper.py отслеживается в git

---
+++ helper.py
@@ -0,0 +1,16 @@
+from faker import Faker
+
+faker = Faker()
+
+
+def generate_registration_data():
+    name = faker.name()
+    email = faker.email()
+    password = faker.password(length=8, special_chars=True, digits=True, upper_case=True, lower_case=True)
+    return name, email, password
+
+def generate_registration_data_too_small_password():
+    name = faker.name()
+    email = faker.email()
+    password = faker.password(length=5)
+    return name, email, password
